c389abb2a93b81163e826ec5e6eab049099d73ac
Anthony Mendoza Reitor <174069095+Anthony-Mendoza-Reitor@users.noreply.github.com>
2024-09-08 13:34:13 -0400

    This marks the completition of the 4 exercise JS
    In this function the filter function is added to remove a value from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, num) {
+    newArray = arr.filter(arrs => arrs !== num);
+    return newArray;
 };
 
 // Do not edit below this line
