5a35490c70cece7baa459f1d0f410cc9a36a469b
Anthony Mendoza Reitor <174069095+Anthony-Mendoza-Reitor@users.noreply.github.com>
2024-09-08 16:07:05 -0400

    This marks the completition of exercise 7 of JS
    Two functions where created to change from Farenheit to Celsius & viceversa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFar) {
+  tempCel = ((5/9)*(tempFar - 32));
+  return (Math.round(tempCel * 100));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCel) {
+  tempFar = ((9/5)*(tempCel + 32));
+  return (Math.round(tempFar * 100));
 };
 
 // Do not edit below this line
