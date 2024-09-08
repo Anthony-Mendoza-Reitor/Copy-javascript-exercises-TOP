2bc791425e99ae60ed7311558dbda3ac8035bba6
Anthony Mendoza Reitor <174069095+Anthony-Mendoza-Reitor@users.noreply.github.com>
2024-09-08 13:01:17 -0400

    This marks the completition of the Exercise 2 JS
    In this exercise a loop whats created to return a string n times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let joint = "";
+    if (num > 0){
+        let count = 0;
+        while (count < num){
+            joint = joint.concat(string);
+            count++;
+        }
+    }
+    return joint;
 };
 
 // Do not edit below this line
