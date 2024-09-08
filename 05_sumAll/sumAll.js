66064feeb7264c07f9a79386a9c6583261cf3260
Anthony Mendoza Reitor <174069095+Anthony-Mendoza-Reitor@users.noreply.github.com>
2024-09-08 13:41:52 -0400

    This marks the completition of exercise 5 of JS
    In this function the numbers are added and the values in betweem them

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let value = 0;
+    for (i = a; i <= b; i++) {
+        value = value + i;
+    }
+    return value;
 };
 
 // Do not edit below this line
