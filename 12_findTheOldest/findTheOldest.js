a464b4aaf41b166cdac5113904a96fd5003b0cf7
Anthony Mendoza Reitor <174069095+Anthony-Mendoza-Reitor@users.noreply.github.com>
2024-09-14 17:02:31 -0400

    This marks the completition of 12 exercise of JS

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,39 @@
-const findTheOldest = function() {
-
+const getAge = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    console.log(death-birth);
+    return death - birth;
+};
+const findTheOldest = function (people) {
+    return people.reduce((a, b) => {
+        const oldestAge = getAge(a.yearOfBirth, a.yearOfDeath);
+        console.log(a);
+        const currentAge = getAge(b.yearOfBirth, b.yearOfDeath);
+        console.log(b);
+        return oldestAge < currentAge ? b : a;
+    });
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
+/* 
+const people = [
+    {
+    name: "Carly",
+    yearOfBirth: 2018,
+    },
+    {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+    },
+    {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+    },
+]
+*/
