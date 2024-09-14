3f5b579f2067a63d541579107791615b87fd101c
Anthony Mendoza Reitor <174069095+Anthony-Mendoza-Reitor@users.noreply.github.com>
2024-09-14 11:30:44 -0400

    This marks the completition of exercise 09 of JS

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    // This is a variable containing all the alphanumerical char
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    const splitCharacters = word 
+    .toLowerCase()
+    .split('')
+    .filter((character)=>alphanumerical.includes(character))
+    .join('');
+    const reversedword = splitCharacters.split('').reverse().join('');
+    return reversedword === splitCharacters;
 };
 
 // Do not edit below this line
