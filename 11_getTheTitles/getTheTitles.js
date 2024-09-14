fad0701cded210628eeb7e47c896cf1c907042aa
Anthony Mendoza Reitor <174069095+Anthony-Mendoza-Reitor@users.noreply.github.com>
2024-09-14 16:27:02 -0400

    This marks the completetion of 11 JS Exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let aLength = books.length;
+    const bucket = [];
+    for (let i = 0; i < aLength; i++){
+        bucket.push(books[i].title)
+    } 
+    return bucket;
 };
 
 // Do not edit below this line
